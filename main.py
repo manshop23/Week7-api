8a3edbed4b5fd8d08909bd1f7840f30b02db8762
manshop23 <mr.ronnakon.pl.61@ubu.ac.th>
2021-12-16 00:46:07 +0700

    เเเ

--- main.py
+++ main.py
@@ -12,13 +12,9 @@ from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
-# @app.post("/login/")
-# async def login(username: str = Form(...), password: str = Form(...)):
-#     return {"username": username}
 
 @app.get("/Ronnakon")
 def Ronnakon():
-    
     return {"Hello"}
 
 @app.get("/len")
@@ -31,27 +27,6 @@ def result(res):
 @app.get("/")
 async def main():
     return 'Hello World'
-
-# @app.post("/health")
-# async def BMI(height: str = Form(...), weight: str = Form(...)):
-#     height = float(height)
-#     weight = float(weight)
-#     h = height / 100
-#     bmi = (weight/(math.pow(h,2)))
-#     if bmi >= 40:
-#         shape = 'Obese Class III'
-#     elif bmi >= 35:
-#         shape = 'Obese Class II'
-#     elif bmi >= 28.5:
-#         shape = 'Obese Class I'
-#     elif bmi >= 23.5:
-#         shape = 'Overweight'
-#     elif bmi >= 18.5:
-#         shape = 'Normal'
-#     else:
-#         shape = 'Mild Thinness'
-#     return {"BMI = ": bmi, "your shape ": shape}
-
 
 @app.get("/test")
 async def test():
