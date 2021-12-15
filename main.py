ea1214eaa8f5c05f981fa83818a22dbfb15ac9a0
manshop23 <mr.ronnakon.pl.61@ubu.ac.th>
2021-12-16 00:54:16 +0700

    yy

--- main.py
+++ main.py
@@ -12,10 +12,8 @@ from fastapi.responses import PlainTextResponse
 
 app = FastAPI()
 
-@app.post("/language/")
-async def langauge(name: str = Form(...), type: str = Form(...)):
-    return {"name": name, "type": type}
-    
+
+
 @app.get("/Ronnakon")
 def Ronnakon():
     return {"Hello"}
